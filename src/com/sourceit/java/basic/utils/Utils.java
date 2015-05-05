fffe89cb932805f5baaa046f2c27ac7044520cd9
alex.z <alexander.zrelykh@creamtec.com>
2015-05-05 16:24:38 +0300

    Utilities

---
+++ src/com/sourceit/java/basic/utils/Utils.java
@@ -0,0 +1,17 @@
+package com.sourceit.java.basic.utils;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Utils {
+
+	public static List<Integer> createSequence(int size){
+		Integer[] arr = new Integer[size];
+
+		for(int i = 0; i < size; i++){
+			arr[i] = i;
+		}
+
+		return Arrays.asList(arr);
+	}
+}
